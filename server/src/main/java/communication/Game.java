9feb47c4d2980327b190b8d8b3572bfc17bda8b7
SponJ <authoritative.jra@gmail.com>
2018-10-08 11:43:07 -0600

    Updated server-side update tracking. The check updates function of the server is now split into 2 functions: checkUpdates(), which takes an authToken and returns the version number for the gameList; and checkGame(), which takes an authToken and gameID and returns the version number for the specific game

--- server/src/main/java/communication/Game.java
+++ server/src/main/java/communication/Game.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 public class Game {
     private String mGameID;
-    private String mGameName;
     private String mHost;
     private int mMaxPlayers;
     private List<Player> mPlayers;
@@ -17,8 +16,8 @@ public class Game {
 
     private boolean misStarted;
 
-    public Game(String mGameName, String mUsername, List<Player> mPlayers, int mMaxPlayers) {
-        this.mGameName = mGameName;
+    public Game(String mGameID, String mUsername, List<Player> mPlayers, int mMaxPlayers) {
+        this.mGameID = mGameID;
         this.mHost = mUsername;
         this.mPlayers = mPlayers;
         this.mMaxPlayers = mMaxPlayers;
@@ -32,14 +31,6 @@ public class Game {
 
     public void setGameID(String mGameID) {
         this.mGameID = mGameID;
-    }
-
-    public String getGameName() {
-        return mGameName;
-    }
-
-    public void setGameName(String mGameName) {
-        this.mGameName = mGameName;
     }
 
     public String getHost() {
--- server/src/test/java/com/obfuscation/server/ServerFacadeTest.java
+++ server/src/test/java/com/obfuscation/server/ServerFacadeTest.java
@@ -3,9 +3,11 @@ package com.obfuscation.server;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import communication.Game;
 import communication.Player;
+import communication.Result;
 
 import static org.junit.Assert.*;
 
@@ -16,6 +18,8 @@ public class ServerFacadeTest {
 
     ServerFacade facade = ServerFacade.getInstance();
 
+    static final String KEY = "masterKey";
+
     @Test
     public void login() throws Exception {
         //Phase 1: test invalid input
@@ -25,8 +29,9 @@ public class ServerFacadeTest {
 
         facade.Register("John", "john");
 
-        //Phase 2: test invalid username/password
+        //Invalid password
         assertEquals(facade.Login("John", "notjohn").isSuccess(), false);
+        //Invalid username
         assertEquals(facade.Login("NotJohn", "notjohn").isSuccess(), false);
 
         //Phase 3: test valid login
@@ -43,19 +48,55 @@ public class ServerFacadeTest {
 
         //Phase 2: test registration and re-registration
         assertEquals(facade.Login("James", "james").isSuccess(), false);
+        //valid registration
         assertEquals(facade.Register("James", "james").isSuccess(), true);
         assertEquals(facade.Login("James", "james").isSuccess(), true);
+        //cannot re-register
         assertEquals(facade.Register("James", "james").isSuccess(), false);
     }
 
     @Test
     public void joinGame() throws Exception {
         //Phase 1: test invalid input
+        List<Player> players = new ArrayList<>();
+        facade.Register("Jill", "Jill");
+        Game game = new Game("Jill's game", "Jill", players, 2);
+        facade.CreateGame(game, "masterKey");
+
+        //Test valid join
+        Result True = new Result(true, null, null);
+        facade.Register("Jack", "jack");
+        assertEquals(facade.JoinGame("Jack", "Jill's game", "masterKey").getErrorInfo(), null);
+
+        //Test invalid (too many players)
+        facade.Register("Harold", "harold");
+        assertEquals(facade.JoinGame("Harold", "Jill's game", "masterKey").isSuccess(), false);
+
+        //Test invalid (game started)
+        Game game5 = new Game("5", "Harold", players, 3);
+        facade.JoinGame("Jack", "5", KEY);
+        assertEquals(facade.JoinGame("Jack", "5", KEY).isSuccess(), false);
+        facade.StartGame("5", KEY);
+
+        assertEquals(facade.JoinGame("Jill", "5", KEY).isSuccess(), false);
+
 
     }
 
     @Test
     public void leaveGame() throws Exception {
+        facade.Register("Bot", "bob");
+        List<Player> list = new ArrayList<>();
+        Game game = new Game("Bot's game", "Bot", list, 2);
+        facade.CreateGame(game, KEY);
+
+        facade.Register("Kyle", "kyle");
+        assertEquals(facade.LeaveGame("Kyle", "Bot's game", KEY).isSuccess(), false);
+
+        facade.JoinGame("Kyle", "Bot's game", KEY);
+        assertEquals(facade.LeaveGame("Kyle", "Bot's game", KEY).getErrorInfo(), null);
+
+
     }
 
     @Test
@@ -66,40 +107,95 @@ public class ServerFacadeTest {
 
         game = new Game(null, null, null, 0);
         assertEquals(facade.CreateGame(game, "masterKey").isSuccess(), false);
-        game.setGameName("Joe's Game");
+        game.setGameID("Joe's Game");
         assertEquals(facade.CreateGame(game, "masterKey").isSuccess(), false);
+        facade.Register("Joe", "joe");
         game.setHost("Joe");
         assertEquals(facade.CreateGame(game, "masterKey").isSuccess(), false);
         game.setPlayers(new ArrayList<Player>());
+        assertEquals(facade.CreateGame(game, "masterKey").isSuccess(), false);
+        game.setMaxPlayers(6);
         assertEquals(facade.CreateGame(game, "masterKey").isSuccess(), false);
         game.setMaxPlayers(3);
         assertEquals(facade.CreateGame(game, "notMasterKey").isSuccess(), false);
         assertEquals(facade.CreateGame(game, "masterKey").isSuccess(), true);
 
-
-
-
-
     }
 
     @Test
     public void startGame() throws Exception {
+        facade.Register("Bob", "bob");
+        List<Player> list = new ArrayList<>();
+        Game game = new Game("Bob's game", "Bob", list, 2);
+        facade.CreateGame(game, KEY);
+
+        assertEquals(facade.StartGame("Bob's game", KEY).isSuccess(), false);
+
+        facade.Register("Hob", "hob");
+        facade.JoinGame("Hob", "Bob's game", KEY);
+
+        assertEquals(facade.StartGame("Bob's game", KEY).getErrorInfo(), null);
+
+
     }
 
     @Test
     public void getGameList() throws Exception {
+        facade.Register("M", "m");
+        List<Player> players = new ArrayList<>();
+
+        Game game1 = new Game("1", "M", players, 2);
+        Game game2 = new Game("2", "M", players, 3);
+
+        facade.CreateGame(game1, KEY);
+        facade.CreateGame(game2, KEY);
+        List<Game> gameList = (ArrayList<Game>) facade.GetGameList(KEY).getData();
+        assertEquals(gameList.size(), 4);
+        assertEquals(gameList.get(2).getGameID(), "1");
     }
 
     @Test
     public void getGame() throws Exception {
+        facade.Register("M", "m");
+        List<Player> players = new ArrayList<>();
+        Game game1 = new Game("1", "M", players, 2);
+        facade.CreateGame(game1, KEY);
+
+        Game game = (Game) facade.GetGame("1", KEY).getData();
+
+        assertEquals(game.getHost(), "M");
     }
 
     @Test
-    public void checkUpdates() throws Exception {
+    public void checkGameList() throws Exception {
+        facade.Register("M", "m");
+        List<Player> players = new ArrayList<>();
+        int up = (Integer) facade.CheckGameList(KEY).getData();
+
+        Game game3 = new Game("3", "M", players, 3);
+        facade.CreateGame(game3, KEY);
+
+        int up2 = (Integer) facade.CheckGameList(KEY).getData();
+
+        assertEquals(up, (up2 - 1));
     }
 
     @Test
     public void checkGame() throws Exception {
+        facade.Register("M", "m");
+        List<Player> players = new ArrayList<>();
+        Game game4 = new Game("4", "M", players, 3);
+        facade.CreateGame(game4, KEY);
+
+        int up = (Integer) facade.CheckGame(KEY, "4").getData();
+
+        facade.Register("N", "n");
+        facade.JoinGame("N", "4", KEY);
+
+        int up2 = (Integer) facade.CheckGame(KEY, "4").getData();
+
+        assertEquals(up, (up2 - 1));
+
     }
 
 }
