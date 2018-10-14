8b65e0af701eb17d84cd301836c2d36a1f6dc47c
haoyucn <haoyu_cn@hotmail.com>
2018-10-13 18:19:38 -0600

    Javadoced function in IServer, please review

--- server/src/main/java/communication/IServer.java
+++ server/src/main/java/communication/IServer.java
@@ -2,21 +2,40 @@ package communication;
 
 import com.sun.org.apache.regexp.internal.RE;
 
+import java.util.ArrayList;
+
 /**
  * Created by jalton on 10/1/18.
  */
 
+
+/**
+ * the interface for ServerFacade and ServerProxy
+ *
+ * @invariant non
+ */
 public interface IServer {
 
     /**
+     * Perform a login process and return the result
+     *
+     * @pre     the id is already registered and password is correctly provided.
+     * @post    a authtoken will be assigned to it, and stored in database
      *
      * @param id        a String containing the username
      * @param password  a String containing the password
+     *
      * @return          a Result object containing the authToken
      */
     Result Login(String id, String password);
 
     /**
+     * Perform a Register process and return a result
+     *
+     * @pre     the id can't be already registerd
+     *          id and password can't be empty
+     * @post    id and password are stored in the database
+     *          id are signed in and a authtoken is assigned to this id
      *
      * @param id        a String containing the username
      * @param password  a String containing the password
@@ -25,6 +44,13 @@ public interface IServer {
     Result Register(String id, String password);
 
     /**
+     * Join the user to a game
+     *
+     * @pre     username of the user already exist
+     *          gameID of the game is already exist, still has empty spots, havent start
+     *          authToken which is still valid and exist, and belong to this user
+     * @post    join the user in the game
+     *
      *
      * @param id        a String object containing the userName
      * @param gameID    a String object containing the gameID
@@ -34,6 +60,14 @@ public interface IServer {
     Result JoinGame(String id, String gameID, String authToken);
 
     /**
+     * let the player leave the game
+     *
+     * @pre     username of the user already exist
+     *          gameID of the game is already exist. Game has the user
+     *          authToken which is still valid and exist and belong to this user
+     *
+     * @post    user leave the game
+     *          game no long have user in the list of active players
      *
      * @param id
      * @param gameID
@@ -43,6 +77,12 @@ public interface IServer {
     Result LeaveGame(String id, String gameID, String authToken);
 
     /**
+     * get game properties as name and number of players allowed in the game, then create it
+     *
+     * @pre     game's name is different from all other games which has been created previously
+     *          number of players allowed in game has to be between 3 to 7
+     *          authToken which is still valid and exist
+     * @post    game is created with all the sepcification of the parameter game.
      *
      * @param game      the game object, containing the game name and max player count
      * @param authToken a String object containing the users authToken
@@ -51,6 +91,12 @@ public interface IServer {
     Result CreateGame(Game game, String authToken);
 
     /**
+     * the game owner can start the game, which will notify all other players in the room
+     *
+     * @pre     the gameID of the game is still in the database
+     *          that game has to have enough amount of players, not over
+     *          the authToken is still valid and its user owns the game
+     * @post    game starts and notify all other players
      *
      * @param gameID    a String object containing the gameID of the game to be started
      * @param authToken a String object containing the user's authToken
@@ -58,8 +104,112 @@ public interface IServer {
      */
     Result StartGame(String gameID, String authToken);
 
+
+    /**
+     * Get a list of command to update a game to the state in the Server
+     *
+     * @pre     authToken has to be valid and belong to a current user in the game.
+     *          gameID is to belong to a game which is already starts
+     *
+     * @post    get a list of command to run to update to the state of game in Server
+     *
+     * @param authToken a String object containing the user's authToken
+     * @param gameID    a String object containing the gameID of the game to be started
+     * @param state     an Integer that record the number of command has been processed
+     * @return          a Result object containing all update commands
+     */
+
+    Result GetUpdates(String authToken, String gameID, int state);
+
+    /**
+     * User claim a route on the map with his cards
+     *
+     * @pre     the game that this user belong has to be already exist and started
+     *          the routeID of the route has to be already in the map and hasnt been claimed
+     *          the cards havent been used
+     *          authToken has to be valid and belong to a current user in the game.
+     *
+     * @post    the route is claimed by the user
+     *
+     * @param routeID    a String object containing ID of the route wanted to claim
+     * @param cards      a List object containing cards used to claim this route
+     * @param authToken  a String object containing the user's authToken
+     * @return           a Result object a boolean to indicate the success or failure
+     */
+    Result ClaimRoute(String routeID, ArrayList<Card> cards, String authToken);
+
+    /**
+     * user draw Train Card from the face up deck
+     *
+     * @pre     the game of this user currently belong is already started
+     *          by the time it call this, it has to be the user's turn, and this function havent been called more than 2 twice in this user round
+     *          the index has to be less than the number of cards remain
+     *
+     * @post    the user get the card he call to draw.
+     *          this card is no longer in the deck of havent-drawn-cards
+     *
+     * @param index     an Integer of the index of the cards in the Train Card deck
+     * @param authToken a String object containing the user's authToken
+     * @return          a Result object contain a card.
+     */
+    Result DrawFaceUpTrainCard(Integer index, String authToken);
+
+    /**
+     * user draw three destination tickets blindly from the deck
+     *
+     * @pre     the game this user currently belong has been started
+     *          it is this player's turn, which player identified by this authToken.
+     *          This function havent been called before in this turn
+     *          This authToken is still valid
+     * @post    there are three dest tickets being removed from the top of DestTicket deck
+     *          and giving to user to choose from
+     *
+     * @param authToken a String object containing the user's authToken
+     * @return          a Result object contain three ticktes.
+     */
+    Result DrawDestTickets(String authToken);
+
+    /**
+     * user put back the tickets he doesnt want to keep
+     *
+     * @pre      the game this user currently belong has been started
+     *          it is this player's turn, which player identified by this authToken.
+     *          DrawDestTickets() function has been called
+     *          This function havent been called before in this turn
+     *          This authToken is still valid
+     *          the number of tickets should be between 0 and 2 .
+     *          non of these tickets are in the DestTicket deck
+     *
+     * @post    a list of tickets being added to the bottom of DestTicket deck
+     *          the card which being sent by DrawDestTickets, but not returned will be assign to this user
+     *
+     * @param tickets   a list of tickets that user want to return
+     * @param authToken a String of unique authToken
+     * @return          a result object containing an boolean indicate of success of this call
+     */
+    Result ReturnDestTickets(List<Ticket>tickets, String authToken);
+
+    /**
+     * user send a message
+     *
+     * @pre     the message can't be empty
+     *          the user who own this authToken has to be in a game
+     *          This authToken is still valid
+     *
+     * @post    the message is added to the list of all messages in the chat.
+     *
+     * @param message   a String that contain the message user want to sent
+     * @param authToken a String of unique authToken
+     * @return
+     */
+    Result SendMessage(String message, String authToken);
+
+    //TODO: functions below should are deprecated
+
     /**
      *
+     *
+     * @pre
      * @param authToken a String object containing the authToken of the user
      * @return          a Result object containing the game list
      */
