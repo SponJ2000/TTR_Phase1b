7b80ae4f3ab6a2fc51f1a19cb7d4afebf0828687
SponJ <authoritative.jra@gmail.com>
2018-10-24 10:30:31 -0600

    Added Messages, finished View interfaces

--- app/src/main/java/com/obfuscation/ttr_phase1b/gameViews/IChatView.java
+++ app/src/main/java/com/obfuscation/ttr_phase1b/gameViews/IChatView.java
@@ -1,8 +1,14 @@
 package com.obfuscation.ttr_phase1b.gameViews;
 
+import java.util.List;
+
+import communication.Message;
+
 /**
  * Created by jalton on 10/24/18.
  */
 
-public class IChatView {
+public interface IChatView {
+    void updateUI();
+    void setMessages(List<Message> messages);
 }
--- app/src/main/java/com/obfuscation/ttr_phase1b/gameViews/IMenuView.java
+++ app/src/main/java/com/obfuscation/ttr_phase1b/gameViews/IMenuView.java
@@ -4,5 +4,7 @@ package com.obfuscation.ttr_phase1b.gameViews;
  * Created by jalton on 10/24/18.
  */
 
-public class IMenuView {
+public interface IMenuView {
+
+
 }
