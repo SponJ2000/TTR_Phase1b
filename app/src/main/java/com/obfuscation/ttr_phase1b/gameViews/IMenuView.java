c8482777348bbca99165764b7fa985a7f811ff8c
SponJ <authoritative.jra@gmail.com>
2018-10-24 10:25:09 -0600

    Added Route and GameMap, working on View interfaces

---
+++ app/src/main/java/com/obfuscation/ttr_phase1b/gameViews/IMenuView.java
@@ -0,0 +1,8 @@
+package com.obfuscation.ttr_phase1b.gameViews;
+
+/**
+ * Created by jalton on 10/24/18.
+ */
+
+public class IMenuView {
+}
---
+++ server/src/main/java/communication/GameMap.java
@@ -0,0 +1,12 @@
+package communication;
+
+import java.util.List;
+
+/**
+ * Created by jalton on 10/24/18.
+ */
+
+public class GameMap {
+    private List<City> cities;
+    private List<Route> routes;
+}
