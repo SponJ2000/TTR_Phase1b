dbb6105162600b842381a56c4bcbd8dd64f69eaf
Iain Lee <icleen@gmail.com>
2018-10-24 10:57:47 -0600

    created presenter classes

---
+++ app/src/main/java/gamePresenters/ChatPresenter.java
@@ -0,0 +1,18 @@
+package gamePresenters;
+
+public class ChatPresenter implements IChatPresenter {
+    @Override
+    public void updateInfo(Object result) {
+
+    }
+
+    @Override
+    public void goBack() {
+
+    }
+
+    @Override
+    public void onSend() {
+
+    }
+}
---
+++ app/src/main/java/gamePresenters/MenuPresenter.java
@@ -0,0 +1,23 @@
+package gamePresenters;
+
+public class MenuPresenter implements IMenuPresenter {
+    @Override
+    public void goBack() {
+
+    }
+
+    @Override
+    public void logout() {
+
+    }
+
+    @Override
+    public void onGameSelection() {
+
+    }
+
+    @Override
+    public void updateInfo(Object result) {
+
+    }
+}
