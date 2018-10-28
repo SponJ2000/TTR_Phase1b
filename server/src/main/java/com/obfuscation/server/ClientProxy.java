cbc186293d7f47e702f1d47ed70f30bef20a9eb8
Urim Jung <urimjung1@gmail.com>
2018-10-28 11:15:08 -0600

    updated IClient interface to have specified functions

--- server/src/main/java/com/obfuscation/server/ClientProxy.java
+++ server/src/main/java/com/obfuscation/server/ClientProxy.java
@@ -1,9 +1,13 @@
 package com.obfuscation.server;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import communication.Card;
+import communication.DestinationTicketCard;
 import communication.IClient;
+import communication.ICommand;
 import communication.Message;
 import communication.Result;
 
@@ -12,6 +16,58 @@ import communication.Result;
  */
 
 public class ClientProxy implements IClient {
+    private Map<GamePlayerPair, List<ICommand>> notSeenCommands = new HashMap();
+    private static final String CLIENT_FACADE = "ClientFacade";
+
+    @Override
+    public void updatePlayerPoints(String gameID, String plyerID, int points) {
+
+    }
+
+    @Override
+    public void updateTrainCards(String gameID, Card trainCard) {
+
+    }
+
+    @Override
+    public void updateTickets(String gameID, DestinationTicketCard destinationTicketCard) {
+
+    }
+
+    @Override
+    public void updateOpponentTrainCards(String gameID, String playerID, int cardNum) {
+
+    }
+
+    @Override
+    public void updateOpponentTrainCars(String gameID, String playerID, int carNum) {
+
+    }
+
+    @Override
+    public void updateOpponentTickets(String gameID, String playerID, int cardNum) {
+
+    }
+
+    @Override
+    public void updateTrainDeck(String gameID, Card faceCards, int downCardNum) {
+
+    }
+
+    @Override
+    public void updateDestinationDeck(String gameID, int cardNum) {
+
+    }
+
+    @Override
+    public void claimRoute(String gameID, String playerID, String routeID) {
+
+    }
+
+    @Override
+    public void updateChat(String gameID, Message m) {
+
+    }
 
     private int version;
     private Map<String, Integer> gameVersion;
@@ -56,8 +112,44 @@ public class ClientProxy implements IClient {
         }
     }
 
-    @Override
-    public void updateChat(Message m) {
+    public Result getNotSeenCommands(String gameID, String playerID) {
+        GamePlayerPair gamePlayerPair = new GamePlayerPair(gameID, playerID);
+        if (notSeenCommands.containsKey(gamePlayerPair)) {
+            if (notSeenCommands.get(gamePlayerPair) != null) {
+                Result result = new Result(true, notSeenCommands.get(gamePlayerPair), null);
+                notSeenCommands.get(gamePlayerPair).clear();
+                return result;
+            }
+        }
+        return new Result(false, null, "Error : gamePlayerPair not found");
+    }
 
+
+
+    public class GamePlayerPair {
+        String gameID;
+        String playerID;
+
+        public String getGameID() {
+            return gameID;
+        }
+
+        public void setGameID(String gameID) {
+            this.gameID = gameID;
+        }
+
+        public String getPlayerID() {
+            return playerID;
+        }
+
+        public void setPlayerID(String playerID) {
+            this.playerID = playerID;
+        }
+
+        public GamePlayerPair(String gameID, String playerID) {
+
+            this.gameID = gameID;
+            this.playerID = playerID;
+        }
     }
 }
--- server/src/main/java/communication/IClient.java
+++ server/src/main/java/communication/IClient.java
@@ -12,5 +12,23 @@ public interface IClient {
     //TODO: this functon should be replaced with function that has specific purpose, like OpponentClaimRoute()...
     void updateGame(String gameID);
 
-    void updateChat(Message m);
+    void updatePlayerPoints(String gameID, String plyerID, int points);
+
+    void updateTrainCards(String gameID, Card trainCard);
+
+    void updateTickets(String gameID, DestinationTicketCard destinationTicketCard);
+
+    void updateOpponentTrainCards(String gameID, String playerID, int cardNum);
+
+    void updateOpponentTrainCars(String gameID, String playerID, int carNum);
+
+    void updateOpponentTickets(String gameID, String playerID, int cardNum);
+
+    void updateTrainDeck(String gameID, Card faceCards, int downCardNum);
+
+    void updateDestinationDeck(String gameID, int cardNum);
+
+    void claimRoute(String gameID, String playerID, String routeID);
+
+    void updateChat(String gameID, Message m);
 }
