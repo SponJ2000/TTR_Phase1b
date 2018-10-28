dbd96c14aac7ba93ff5a9e0b4f896af857ec1d59
haoyucn <haoyu_cn@hotmail.com>
2018-10-27 20:32:36 -0600

    Added trainCarCard.java, and TrainCarColor.java

---
+++ server/src/main/java/communication/TrainCarCard.java
@@ -0,0 +1,13 @@
+package communication;
+
+/**
+ * Created by hao on 10/27/18.
+ */
+
+public class TrainCarCard {
+    TrainCarCardColor trainCarCardColor;
+
+    public TrainCarCard(TrainCarCardColor trainCarCardColor) {
+        this.trainCarCardColor = trainCarCardColor;
+    }
+}
---
+++ server/src/main/java/communication/TrainCarCardColor.java
@@ -0,0 +1,9 @@
+package communication;
+
+/**
+ * Created by hao on 10/27/18.
+ */
+
+public enum TrainCarCardColor {
+    PURPLE, BLUE, ORANGE, WHITE, GREEN, YELLOW, BLACK, RED;
+}
